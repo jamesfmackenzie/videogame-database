0e8902c22c3126f87b7e63c351223d444f698416
James Mackenzie <jim@lilandjim.com>
2017-04-02 01:16:22 +0100

    Add database browser

---
+++ database-browser/routes/game.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+
+var gameDatabase = require('../../videogame-database.json');
+
+
+
+/* GET */
+router.get('/:id', function (req, res, next) {
+    var game = _.find(gameDatabase, function (o) {
+        return o.id === req.params.id
+    });
+
+    var keys = _.sortBy(_.filter(Object.keys(game), function (key) {
+        return key != "images" && key != "id" && key != "title";
+    }), function (key) {
+        return key.toLowerCase();
+    });
+
+    res.render('game', {
+        id: game.id,
+        title: game.title,
+        platform: game.platform,
+        images: game.images,
+        game: game,
+        keys: keys
+    });
+});
+
+module.exports = router;
---
+++ database-browser/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('lodash');
+
+var gameDatabase = require('../../videogame-database.json');
+
+var platforms = _.sortBy(_.map(_.uniqBy(gameDatabase, 'platform'), 'platform'), function (g) {
+  return g.toLowerCase();
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    _: _,
+    title: 'Videgame Database',
+    size: gameDatabase.length,
+    games: gameDatabase,
+    platforms: platforms
+  });
+});
+
+module.exports = router;
